349c416ecbc070108da0b922f35306a7d340b845
bartmanner <gijs.bartman@gmail.com>
2022-07-23 18:04:30 +0200

    Add colorpicker and color functionality

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const DEFAULT_COLOR = '#333333'
-const DEFAULT_SIZE = 16
+const DEFAULT_SIZE = 64
+
+let currentColor = DEFAULT_COLOR
+let currentSize = DEFAULT_SIZE
+
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
 
 const grid = document.getElementById('grid')
+const colorPicker = document.getElementById('colorPicker')
+
+colorPicker.oninput = (e) => setCurrentColor (e.target.value)
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -9,8 +23,26 @@ function setupGrid(size) {
 
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-item')
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        if (i === 0) {
+            gridElement.style.borderRadius = '10px 0 0 0'
+        } else if (i === size - 1) {
+            gridElement.style.borderRadius = '0 10px 0 0'
+        } else if (i === size * size - 1) {
+            gridElement.style.borderRadius = '0 0 10px 0'
+        } else if (i === size * size - size) {
+            gridElement.style.borderRadius = '0 0 0 10px'
+        }
         grid.appendChild(gridElement)
     }
+}
+
+function changeColor(e) {
+    console.log()
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = currentColor
 }
 
 setupGrid(DEFAULT_SIZE)
